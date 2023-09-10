9c95235d65dcddb8553417bfcdb3d1fd815db3ad
Qypol342 <Qypol342@gmail.com>
2023-09-10 18:18:37 +0200

    start

---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+
+<!--
+
+    <div class="flex flex-col gap-5 p-5 w-screen min-h-screen bg-cyan-200 md:h-screen">
+    <div class="flex z-30 w-full h-28 bg-yellow-300 rounded-lg"></div>
+    <div class="flex z-30 gap-5 w-full h-full rounded-lg">
+        <div class="rounded-lg bg-purple-300 w-full h-full min-w-[8rem]"></div>
+        <div class="rounded-lg bg-purple-300 w-full h-full min-w-[8rem]"></div>
+        <div class="flex gap-5 flex-col rounded-lg bg-purple-300 w-full h-full min-w-[8rem]">
+            <div class="w-full h-1/2 bg-green-100 rounded-lg"></div>
+            <div class="w-full h-1/2 bg-green-100 rounded-lg"></div>
+
+        </div>
+
+    </div>
+</div>
+-->
+<div class="flex overflow-hidden flex-col p-5 w-screen min-h-screen bg-cyan-200 opacity-70 md:h-screen">
+    <div class="flex z-30 w-full h-28 bg-yellow-300 rounded-lg"></div>
+    <div class="flex z-30 w-full h-full rounded-lg">
+        <div class="relative flex-shrink-0 pr-5 h-full left-fit">
+            <div class="flex pr-5 w-full h-full t-0 l-0 bg-stone-500"></div>
+            <div class="flex gap-5 pt-5 w-full h-full">
+                <div class="flex w-full h-full bg-orange-300 rounded-lg"></div>
+                <div class="flex w-full h-full bg-orange-300 rounded-lg"></div>
+
+            </div>
+  
+        </div>
+        <div class="flex-grow bg-red-200">
+            <div class="flex gap-5 flex-col rounded-lg bg-purple-300 w-full h-full min-w-[8rem]">
+     
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<!--
+<div class="flex overflow-hidden absolute top-0 left-0 z-10 gap-5 p-5 w-full min-h-screen md:h-full">
+    <div class="w-full h-full bg-yellow-200 rounded-lg"></div>
+</div>
+<div class="flex overflow-hidden absolute top-0 left-0 z-20 flex-col p-5 w-full min-h-screen md:h-full">
+    <div class="flex w-full h-28 rounded-lg"></div>
+    <div class="flex gap-5 w-full h-full rounded-lg z-200">
+        <div class="rounded-lg rounded-l-none flex-grow bg-cyan-200 w-2/3 h-full min-w-[8rem]"></div>
+        <div class="rounded-lg bg-purple-300 w-1/3 h-full min-w-[8rem]"></div>
+
+    
+    </div>
+</div>
+
+-->

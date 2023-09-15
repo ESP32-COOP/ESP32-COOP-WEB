aefa5e7aefce4da7a8f14ad80cb98906ba500f96
Qypol342 <Qypol342@gmail.com>
2023-09-15 21:51:59 +0200

    start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,29 @@
-<div class="flex relative flex-col items-end p-5 w-screen h-screen bg-coop-blue-100">
-    <div class="w-full h-24"></div>
-    <div class="flex items-end w-full h-full felx">
-        <div class="w-[100%] h-full bg-coop-blue-50 rounded-2xl rounded-br-none"></div>
-        <div class="relative w-[80%] h-1/2 bg-coop-blue-50 rounded-2xl rounded-bl-none rounded-tl-none inv-rounded"></div>
+<div class="flex relative flex-col items-end p-3 w-screen h-screen bg-coop-blue-50">
+    <div class="flex gap-2 items-center pb-3 w-full h-16">
+        <img class="w-auto h-full" src="/logo.svg" alt="coop door esp32 logo">
+        <h1 class="text-[2.5rem] uppercase">esp32 coop door</h1>
+        <a href="https://coop-door.vercel.app/" class="px-3 py-1 ml-auto text-white bg-gradient-to-r rounded-full from-slate-700 to-slate-900">Acces App</a>
+    </div>
+    <div class="flex w-full h-full felx">
+        <div class="w-[100%] h-full bg-coop-blue-100 rounded-2xl rounded-br-none"></div>
+        <div class="w-[80%] h-full flex flex-col">
+                <div class="flex z-20 items-end p-5 w-full h-1/2">
+                    <h2 class="text-[3.1rem] leading-tight ">The perfect <span class="uppercase">COOP DOOR</span> 
+                        for you</h2>
+                </div>
+            <div class="flex relative flex-col gap-3 p-3 pl-1 w-full h-1/2 rounded-2xl rounded-tl-none rounded-bl-none bg-coop-blue-100 inv-rounded">
+                <div class="flex items-end p-5 w-full h-2/3 rounded-2xl bg-coop-blue-50">
+                    <p class="text-xl font-bold">Never worry about your chicken</p>
+                    <img class="w-auto h-3/4" src="/esp32.svg" alt="esp32">
+                </div>
+                <div class="flex items-end p-5 w-full h-1/3 rounded-2xl bg-coop-blue-50">
+                    <p class="text-lg">Buy your how here</p>
+                    <img class="w-auto ml-auto  h-full max-w-[5rem]" src="/arrow.svg" alt="arrow">
+
+                </div>
+            </div>
+
+        </div>
     </div>
 
 

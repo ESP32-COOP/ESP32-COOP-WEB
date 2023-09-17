a19b56fd860910f43775e8bc17a03fc8cce84bd9
Qypol342 <Qypol342@gmail.com>
2023-09-17 20:09:17 +0200

    better responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
-<div class="flex relative flex-col items-end p-3 w-screen lg:h-screen bg-coop-blue-50">
+<div class="flex relative flex-col items-end p-5 w-screen lg:h-screen bg-coop-blue-50">
     <div class="flex gap-2 items-center pb-3 w-full h-20">
         <img class="w-auto h-full" src="/logo.svg" alt="coop door esp32 logo">
         <h1 class="text-2xl sm:text-[2.5rem] uppercase">esp32 coop door</h1>
-        <a href="https://coop-door.vercel.app/" class="px-12 py-2 ml-auto text-xl text-white bg-gradient-to-r rounded-full from-slate-700 to-slate-900">Acces App</a>
+        <a href="https://coop-door.vercel.app/" class="px-5 py-2 ml-auto text-sm text-white whitespace-nowrap bg-gradient-to-r rounded-full md:text-xl md:px-12 md:py-2 from-slate-700 to-slate-900">Acces App</a>
     </div>
     <div class="flex overflow-hidden flex-col-reverse w-full h-full lg:flex-row">
-        <div class="flex flex-col gap-5 p-5 w-full h-full rounded-2xl rounded-br-none sm:flex-row bg-coop-blue-100">
+        <div class="flex flex-col gap-5 p-5 w-full h-full rounded-2xl rounded-tl-none rounded-tr-none lg:rounded-tr-2xl lg:rounded-br-none sm:flex-row bg-coop-blue-100 md:rounded-tl-2xl">
             <div class="flex flex-col justify-end p-5 w-full h-full bg-white rounded-2xl">
                 <div class="flex overflow-hidden justify-center items-end w-full h-full">
                     <img class="w-auto max-w-full h-full max-h-2/3" src="/chicken.svg" alt="chicken"/>
@@ -13,13 +13,13 @@
                 <p class="text-[2.2rem] h-fit align-bottom">Loved by all chicken & their owner</p>
             </div>
             <div class="flex flex-col gap-5 w-full h-auto rounded-xl lg:h-full">
-                <div class="flex flex-col gap-2 justify-end p-5 w-full h-1/2 bg-gradient-to-r rounded-2xl from-slate-700 to-slate-900">
+                <a href="https://coop-door.vercel.app/" class="flex flex-col gap-2 justify-end p-5 w-full h-1/2 bg-gradient-to-r rounded-2xl from-slate-700 to-slate-900">
                     <div>
                         <img class="w-auto h-18" src="/addFile.svg" alt="add document"/>
                     </div>
                     <p class="text-[1.5rem] lg:text-[2rem] font-bold text-white">Contribute to <br> the project !</p>
 
-                </div>
+                </a>
                 <div class="flex flex-col gap-2 p-5 w-full h-1/2 bg-white rounded-2xl">
                     <div class="flex justify-center items-center w-full h-full">
                         <img src="/plus.svg" alt="plus" />
@@ -37,13 +37,13 @@
                         <span class="uppercase">COOP DOOR</span> <br>
                         for you</h2>
                 </div>
-            <div class="flex relative flex-col gap-3 p-3 pl-1 w-full h-1/2 rounded-2xl rounded-tl-none rounded-bl-none bg-coop-blue-100 inv-rounded">
-                <div class="flex items-end p-5 w-full h-2/3 bg-white rounded-2xl">
+            <div class="flex relative flex-col gap-5 p-5 pl-5 w-full h-1/2 rounded-2xl rounded-br-none rounded-bl-none md:inv-rounded-bl lg:inv-rounded-bl-none lg:rounded-bl-2xl lg:pl-1 lg:rounded-tl-none bg-coop-blue-100 lg:inv-rounded-tl">
+                <div class="flex items-end p-4 w-full h-2/3 bg-white rounded-2xl">
                     <p class="text-xl lg:text-[2rem] leading-tight font-bold">Never worry about <br> your chicken</p>
                     <img class="ml-auto w-auto h-3/4" src="/esp32.svg" alt="esp32">
                 </div>
-                <div class="flex items-center p-5 w-full bg-white rounded-2xl lg:h-1/3">
-                    <p class="text-xl lg:text-[2.3rem]">Buy your how here</p>
+                <div class="flex items-center p-3 w-full bg-white rounded-2xl lg:h-1/3">
+                    <p class="text-xl lg:text-[1.5rem]">Buy your how here</p>
                     <img class="w-auto ml-auto  h-full max-w-[5rem]" src="/arrow.svg" alt="arrow">
 
                 </div>

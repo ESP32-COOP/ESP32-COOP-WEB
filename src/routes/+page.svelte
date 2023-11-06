5c7e885e9af35107ce1b4c05a229b9f6464f9920
qypol342 <qypol342@gmail.com>
2023-11-06 14:00:25 +0100

    fix(style) css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,11 +20,11 @@
                     <p class="text-[1.5rem] lg:text-[2rem] font-bold text-white">Contribute to <br> the project !</p>
 
                 </a>
-                <a href="https://github.com/ESP32-COOP/ESP32-COOP-DOC" class="flex flex-col gap-2 p-5 w-full bg-white rounded-2xl h-fit user-drag-none">
+                <a href="https://github.com/ESP32-COOP/ESP32-COOP-DOC" class="flex flex-col gap-2 p-5 w-full h-1/2 bg-white rounded-2xl user-drag-none">
                     <div class="flex justify-center items-center w-full h-full">
                         <img src="/plus.svg" alt="plus" class="user-drag-none" />
                     </div>
-                    <div  class="flex flex-col justify-center items-center w-full h-full">
+                    <div  class="flex flex-col justify-center items-center w-full h-fit">
                         <p class="text-2xl font-bold w-fit">Build it !</p>
                         <p class="font-bold text-gray-500 w-fit">tailored to your needs</p>
                     </div>

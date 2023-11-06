437ef34f96ca9a5b2fe4211298906c039bd2281e
qypol342 <qypol342@gmail.com>
2023-11-06 13:55:00 +0100

    style(layout) text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,7 +32,7 @@
             </div>
         </div>
         <div class="lg:w-[80%] h-full flex flex-col md:flex-row lg:flex-col">
-                <div class="flex z-20 items-end p-5 w-full h-1/2">
+                <div class="flex z-20 items-end p-5 mt-auto w-full h-1/2">
                     <h2 class="text-[3.1rem] lg:text-[4rem] xl:text-[4.5rem] 2xl:text-[6rem]  leading-tight ">The perfect <br>
                         <span class="uppercase">COOP DOOR</span> <br>
                         for you</h2>

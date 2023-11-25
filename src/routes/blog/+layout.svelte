6aa2ebfd06a52092763d7514b9aff506eeb2858d
qypol342 <qypol342@gmail.com>
2023-11-25 22:43:23 +0100

    feat(blog) list

---
+++ src/routes/blog/+layout.svelte
@@ -0,0 +1,20 @@
+<div
+  class="flex relative flex-col items-end p-5 w-screen h-screen bg-coop-blue-50 bg-tempalte"
+>
+  <div class="flex gap-2 items-center pb-3 w-full h-20">
+    <img
+      class="w-auto h-full user-drag-none"
+      src="/logo.svg"
+      alt="coop door esp32 logo"
+    />
+    <h1 class="sm:text-[2rem] md:text-[2.5rem] uppercase">esp32 coop door</h1>
+    <a
+      href="https://coop-door.vercel.app/"
+      class="px-5 py-2 ml-auto text-sm text-white whitespace-nowrap bg-gradient-to-r rounded-full user-drag-none md:text-xl md:px-12 md:py-2 from-slate-700 to-slate-900"
+      >Acces App</a
+    >
+  </div>
+  <div class="flex overflow-hidden flex-col-reverse w-full h-full lg:flex-row">
+    <slot />
+  </div>
+</div>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,27 +1,53 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    import { formatDate } from '$lib/utils'
+  import type { PageData } from "./$types";
+  import { formatDate } from "$lib/utils";
 
-    import * as config from '$lib/config';
-    
-    export let data: PageData;
+  import * as config from "$lib/config";
+
+  export let data: PageData;
 </script>
 
-
 <svelte:head>
-    <title>
-        {config.title}
-    </title>
+  <title>
+    {config.title}
+  </title>
 </svelte:head>
 
-<section>
-	<ul class="posts">
-		{#each data.blogs as blog}
-			<li class="post">
-				<a href={blog.slug} class="title">{blog.title}</a>
-				<p class="date">{formatDate(blog.date)}</p>
-				<p class="description">{blog.description}</p>
-			</li>
-		{/each}
-	</ul>
+<section class="flex flex-col w-full h-full">
+	<div class="flex h-fit w-fit">
+		<h1 class="text-[2rem] pl-10 font-bold">Blog list:</h1>
+	  </div>
+  <div class="flex justify-center w-full h-full">
+    
+
+    <ul class="flex h-full w-fit">
+      {#each data.blogs as blog}
+        <a
+          href="/blog/{blog.slug}"
+          class="flex min-w-[20rem] flex-col gap-2 justify-start p-5 w-full h-40 bg-gradient-to-r rounded-2xl user-drag-none from-slate-700 to-slate-900"
+        >
+          <p class="text-[1.5rem] lg:text-[1.9rem] font-bold text-white">
+            {blog.title}
+          </p>
+
+          <p class="text-[1rem] lg:text-[1.2rem] text-white">
+            {blog.title}
+          </p>
+
+          <div class="flex gap-5 items-center mt-auto">
+            <p class="text-sm text-white">
+              {formatDate(blog.date)}
+            </p>
+            <div class="flex overflow-x-auto gap-2 ml-auto">
+              {#each blog.categories as categorie}
+                <p class="p-1 py-0 text-sm text-black rounded-lg bg-slate-400">
+                  {categorie}
+                </p>
+              {/each}
+            </div>
+          </div>
+        </a>
+      {/each}
+    </ul>
+  </div>
 </section>
